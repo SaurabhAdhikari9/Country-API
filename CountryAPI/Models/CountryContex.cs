b445634f15aaf5bf7893d41877c0f0c082fa1b88
Saurabh Adhikari <sauravadhikari98@gmail.com>
2022-03-26 08:45:12 +0545

    Country API

---
+++ CountryAPI/Models/CountryContex.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+namespace CountryAPI.Models
+{
+    public class CountryContex : DbContext
+    {
+        public CountryContex(DbContextOptions<CountryContex> options)
+            : base(options) 
+        { 
+        }
+
+        public DbSet<CountryItem> CountryItems { get; set; } = null!;
+    }
+}
---
+++ CountryAPI/Models/CountryItem.cs
@@ -0,0 +1,11 @@
+﻿namespace CountryAPI.Models
+{
+    public class CountryItem
+    {
+        //public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int salary { get; set; }
+    }
+}
